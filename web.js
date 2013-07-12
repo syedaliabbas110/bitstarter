254ef5ff3a1d142a39be09daa81b6ee79c81cf0b
syedaliabbas110 <syedaliabbas110@gmail.com>
2013-07-12 16:55:07 +0500

    Reading the text from index.html

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file1 = fs.readFileSync('index.html');
+
+var buffer1 = new Buffer(20);
+
+buffer1.write(file);
+
+var string1 = buffer1.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string1);
 });
 
 var port = process.env.PORT || 5000;
